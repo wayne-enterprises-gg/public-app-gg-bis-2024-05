aad2a08b037f8a12b2f80359f2b8ff683285c285
Lucius Fox <lucius-fox-gg@protonmail.com>
2024-07-03 23:03:37 +0200

    Updating app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import antigravity
 
-SEED=ce7c1cab-71fb-41eb-96a7-25b16e943402
+client_id=3MVG9O2Ky9itsJknXByfhaNh1Z8FUxOBSMwMk29m5Ml20LO4ubDTdUahr_B0XazCUm5TjWKJgprwYCUsgG_qj
+client_secret=2991381630310146316
 
 def main():
     antigravity.fly()
